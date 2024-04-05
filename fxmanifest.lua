fc53ba00c72b176d9996a406a22cd3dcab08ac23
kwarktopiadev <kwarktopiadevelopment@gmail.com>
2024-04-06 00:14:07 +0200

    Update: sg-firstpersonshooting :gun: - fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,19 @@
+
 fx_version 'cerulean'
 game 'gta5'
-client_script{ '@ox_lib/init.lua', 'main.lua', 'config/client.lua'}
 
 lua54 'yes'
+
+-- Information
+author 'Scripto Goat'
+description 'SG First Person Shooting'
+version '1.0.0'
+
+-- Client
+client_script { 
+    '@ox_lib/init.lua', 
+    'main.lua', 
+    'config/client.lua'
+}
+
 use_experimental_fxv2_oal 'yes'
